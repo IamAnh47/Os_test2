69bd44c521979053647e92ab468e152e6c1484a1
Tuan Anh <anh.phanitskyye@hcmut.edu.vn>
2024-12-09 22:42:04 +0700

    Anh

---
+++ src/queue.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
+}
+
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+        /* TODO: put a new process to queue [q] */
+        if (q == NULL || proc == NULL)
+                return;
+        if (q->size < MAX_QUEUE_SIZE)
+        {
+                q->proc[q->size++] = proc;
+        }
+        else
+        {
+                printf("Queue full.\n");
+        }
+}
+
+struct pcb_t *dequeue(struct queue_t *q)
+{
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to remove it from q
+         * */
+        if (empty(q))
+        {
+                return NULL;
+        }
+        struct pcb_t *temp = q->proc[0];
+        for (int i = 0; i < q->size - 1; i++)
+        {
+                q->proc[i] = q->proc[i + 1];
+        }
+        q->proc[q->size - 1] = NULL;
+        q->size = q->size - 1;
+        return temp;
+}
